cdaedb8f409f1f288d8bef42fea54b4eb5c667cd
mardeureu <siixx@hanmail.net>
2020-06-29 16:37:54 +0900

    /

--- asset/js/index_en.js
+++ asset/js/index_en.js
@@ -1,5 +1,3 @@
-//canvas
-var canvas = document.getElementById('icanvas');
-const ctx = canvas.getContext('2d');
-
-ctx.fillStyle();
+window.addEventListener('DOMCotentLoaded',function(){
+    
+});//END
