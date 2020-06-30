a0f15ef19d9f98a3e1ed1183850279dbd14f16d0
mardeureu <siixx@hanmail.net>
2020-06-30 18:33:46 +0900

    main modify

--- asset/js/index_en.js
+++ asset/js/index_en.js
@@ -1,3 +1,28 @@
-window.addEventListener('DOMCotentLoaded',function(){
-    
+window.addEventListener('DOMContentLoaded', function(){
+    //swiper slide
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 15000,
+            stopOnLastOnide: false,
+            disableOn: false,
+            reverseDirection: true,
+            WaitForTransition: true,
+        },
+        keyboard: {
+            enabled: true,
+          },
+        loop: true,
+        });//swiper end
+
 });//END
+
