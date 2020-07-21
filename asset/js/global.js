a55a03605a3eeff13a85362550c4cba1d3387003
mardeureu <siixx@hanmail.net>
2020-07-21 18:31:28 +0900

    mdf

--- asset/js/global.js
+++ asset/js/global.js
@@ -16,16 +16,36 @@ window.addEventListener('DOMContentLoaded', function(){
   window.addEventListener('scroll', function() {
     let subHead2 = document.querySelector('h2');
     let subPara = document.querySelector('.subpage-head p');
-    // const subHead3 = document.querySelector('h3');
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      subHead2.className = 'headscroll';
-      subPara.className = 'subParaScroll';
-    } else {
-      subHead2.className = '';
-      subPara.className = '';
-    }
-  
-  });
+    let subHead3 = document.querySelector('.prod-content-head h3');
+    let subArtistName = document.querySelector('.productions .prod-content-head dl');
+    let subArtistPara = document.querySelector('.prod-content-head p');
+    if (document.contains(subHead2) || document.contains(subPara)) {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        subHead2.className = 'headscroll';
+        subPara.className = 'subParaScroll';
+      } else {
+        subHead2.className = '';
+        subPara.className = '';
+      }
+    } else if (document.contains(subHead3) && document.contains(subArtistName)) {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        subHead3.className = 'headscroll';
+        subArtistName.className = 'headscroll';
+      } else {
+        subHead3.className = '';
+        subArtistName.className = '';
+      }
+    }  else {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        subHead3.className = 'headscroll';
+        subArtistPara.className = 'headscroll';
+      } else {
+        subHead3.className = '';
+        subArtistPara.className = '';
+      }
+    } 
+
+  });//head scroll event end
 
 
 });//END
