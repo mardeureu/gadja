c45012080549f984ab944d0066296ee11a59c35e
mardeureu <siixx@hanmail.net>
2020-10-13 00:14:25 +0900

    s
    s

--- asset/data/ajax.js
+++ asset/data/ajax.js
@@ -1,44 +1,5 @@
-//url을 조건으로 그 url과 같은 경우 해당 내용을 로드하기
-
-var curURL = location.href;
-console.log("curURL : " + curURL);
-
-if (Boolean(curURL.indexOf('Letmechangeyourname'))) {
-  console.log("success");
-}
-
-//console.log();
-
-// //element
-// var elemTitle = document.querySelector(".prod-content-head h3");
-
-// function loadDoc() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       func(this);
-//     }
-//   };
-//   xhttp.open("GET", "../../../asset/dataproductions_sub.json", true);
-//   xhttp.send();
-// }
-
-// function func(dataDoc) {
-//   var i;
-//   var dataDoc;
-//   //data
-//     var dataLoot = dataDoc.productions.EunMeAhn;
-//     var dataTitle = dataLoot.title[0];
-
-//     console.log(dataLoot + dataTitle);
-
-//     // data append
-//     // title
-//     dataTitle.elemTitle.nodeValue;
-// }
-
-
-function loadDoc() {
+//XHR func reqListener
+function reqListener() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -48,19 +9,20 @@ function loadDoc() {
     }
   };
   xmlhttp.open("GET", "../../../asset/data/productions_sub.json", true);
+  xmlhttp.responseType = "text";
   xmlhttp.send();
-}//loadDoc Func
+  console.log("reqListener test: " + xmlhttp);
+}//reqListener Func
+reqListener();
 
 //productions subpage
 function prodCFunc(xmlhttp) {
+  //object variable
   var i;
   var dataDoc = xmlhttp;
-  var perseObj = JSON.parse(dataDoc.reponseText);
+  var perseObj = JSON.parse(dataDoc.responseText);
   var dataLoot = perseObj.EunMeAhn;
- 
-  console.log(dataTitle);
-
-  //DOM node resTitle
+  //node variable
   var resTitle = document.querySelector('.productions .prod-content-head h3');
   var resArtist = document.querySelector('.productions .prod-content-head dl dd');
   var resMain = document.querySelector('.productions .tralier');
@@ -68,10 +30,8 @@ function prodCFunc(xmlhttp) {
   var resDesc = document.querySelector('.productions .prod-content-wrap p');
   var resCredit = document.querySelector('.productions .credit');
   var resSlideImg = document.querySelector('.slide-list');
-
-  //data
-  
-  var dataTitle = dataLoot.title;
+  //data variable
+  var dataTitle = dataLoot[0].title;
 
   (function() {
     if (resTitle.innerText == "") {
@@ -79,16 +39,17 @@ function prodCFunc(xmlhttp) {
     } else {
       resTitle.textContent = "";
     }
-    console.log(dataTitle);
-  })()
-  // var i;
-  // for (i = 0; i < dataTitle.length; i++) {
-  //   var dataTitle = dataLoot.title[i];
-  //   resTitle.textContent = dataTitle[i];
-  // }
+    console.log("prodCFunc test : " + dataTitle);
+  })();
+
 } //prodCFunc Func
-//  console.log(location.hash.slice(1));
-loadDoc();
 
 //dymamic create url page func
+//url conditional
+//  console.log(location.hash.slice(1));
+var curURL = location.href;
+console.log("curURL : " + curURL);
 
+if (Boolean(curURL.indexOf('Letmechangeyourname'))) {
+  console.log("success");
+}
